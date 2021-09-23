4c361d90301ddd27058b7f6fb0dcd0ce2ed1801c
屋大维 <40227914+RanDengShiFu@users.noreply.github.com>
2021-09-23 20:13:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,9 +13,5 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-git clone https://github.com/kiddin9/openwrt-bypass.git /workdir/openwrt/package/openwrt-bypass
-git clone https://github.com/kiddin9/openwrt-packages.git /workdir/openwrt/package/openwrt-packages
 
-./scripts/feeds update -a &&./scripts/feeds install -a;
-make -j32 download;
 echo "依赖包下载完成"
